d7d93c37207e021dd85da48d60f1ae1716692a5e
egemenongel <egemenongel@hotmail.com>
2022-08-16 19:31:25 +0300

    Create home body UI

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:world_clocks/core/extensions/context_extension.dart';
 import 'package:world_clocks/features/home/cubit/home_cubit.dart';
 import 'package:world_clocks/features/home/service/home_service.dart';
+import 'package:world_clocks/features/timezone_detail/view/timezone_detail_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -12,8 +14,51 @@ class HomeView extends StatelessWidget {
       create: (context) => HomeCubit(HomeService()),
       child: Scaffold(
         appBar: AppBar(),
-        body: Container(),
+        body: _buildBody(),
       ),
     );
   }
+}
+
+BlocBuilder<HomeCubit, HomeState> _buildBody() {
+  return BlocBuilder<HomeCubit, HomeState>(
+    builder: (context, state) {
+      if (state is HomeLoading) {
+        return const Center(child: CircularProgressIndicator());
+      } else if (state is HomeLoaded) {
+        return RefreshIndicator(
+          onRefresh: () async => context.read<HomeCubit>().fetchAreas(),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: state.response
+                  .map((timezone) => _buildAreaTile(context, timezone))
+                  .toList(),
+            ),
+          ),
+        );
+      } else {
+        return const Center(
+          child: Text("ERROR"),
+        );
+      }
+    },
+  );
+}
+
+Widget _buildAreaTile(BuildContext context, String timezone) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+    child: InkWell(
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => TimezoneDetailView(timezone: timezone))),
+      child: ListTile(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        tileColor: context.colors.primary,
+        title: Text(timezone.replaceAll('/', ', ').replaceAll('_', ' ')),
+        trailing: const Icon(Icons.keyboard_arrow_right_rounded),
+      ),
+    ),
+  );
 }
