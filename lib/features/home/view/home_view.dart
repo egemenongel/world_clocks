1c67a125fbe3acf13bb4b5cceff3426d5fc02b75
egemenongel <egemenongel@hotmail.com>
2022-08-16 19:29:24 +0300

    Implement provider to home

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:world_clocks/features/home/cubit/home_cubit.dart';
+import 'package:world_clocks/features/home/service/home_service.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+    return BlocProvider(
+      create: (context) => HomeCubit(HomeService()),
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+      ),
     );
   }
 }
